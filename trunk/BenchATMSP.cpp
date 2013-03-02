7083cf22e724fdd1a68c1886947cfbe9e9c6c38d
partow <partow@5a969881-007f-eb33-5950-2d2997f90778>
2013-03-02 02:40:02 +0000

    Fixes to ATMSP when certain expressions cause a failure during parsing.

--- trunk/BenchATMSP.cpp
+++ trunk/BenchATMSP.cpp
@@ -11,55 +11,55 @@ using namespace std;
 
 //-------------------------------------------------------------------------------------------------
 BenchATMSP::BenchATMSP()
-  :Benchmark()
+: Benchmark()
 {
-  m_sName = "atmsp 1.0.3";
+   m_sName = "atmsp 1.0.3";
 }
 
 //-------------------------------------------------------------------------------------------------
-std::string BenchATMSP::replaceAll(std::string result, 
-                                   const std::string& replaceWhat, 
+std::string BenchATMSP::replaceAll(std::string result,
+                                   const std::string& replaceWhat,
                                    const std::string& replaceWithWhat)
 {
-  int start = 0;
-  while(1)
-  {
-    int pos = result.find(replaceWhat, start);
-    
-    if (pos==-1) 
-      break;
+   int start = 0;
+   while(1)
+   {
+      int pos = result.find(replaceWhat, start);
 
-    result.replace(pos,replaceWhat.size(),replaceWithWhat);
-    start = pos + replaceWithWhat.length();
-  }
-  return result;
+      if (pos==-1)
+         break;
+
+      result.replace(pos,replaceWhat.size(),replaceWithWhat);
+      start = pos + replaceWithWhat.length();
+   }
+   return result;
 }
 
 //-------------------------------------------------------------------------------------------------
 void BenchATMSP::PreprocessExpr(std::vector<std::string> &vExpr)
 {
-  // atmsp is using $ to mark a constant
-  for (std::size_t i=0; i<vExpr.size(); ++i)
-  {
-    PreprocessExpr(vExpr[i]);
-    //string s = vExpr[i];
-    //s = replaceAll(s, string("pi"), string("$pi"));
-    //s = replaceAll(s, string("-e*"), string("-$e*"));
-    //s = replaceAll(s, string("/e*"), string("/$e*"));
-    //s = replaceAll(s, string("(e*"), string("($e*"));
-    //s = replaceAll(s, string("-e/"), string("-$e/"));
-    //s = replaceAll(s, string("e^"), string("$e^"));
-    //s = replaceAll(s, string("+e)"), string("+$e)"));
-    //s = replaceAll(s, string("(e+"), string("($e+"));
-    //s = replaceAll(s, string("(e-"), string("($e-"));
-    //s = replaceAll(s, string("(-e"), string("(-$e"));
-    //s = replaceAll(s, string("-e)"), string("-$e)"));
-    //s = replaceAll(s, string("(e)"), string("($e)"));
-    //s = replaceAll(s, string("/e)"), string("/$e)"));
-    //s = replaceAll(s, string("/e/"), string("/$e/"));
-    //s = replaceAll(s, string("*e+"), string("*$e+"));
-    //vExpr[i] = s;
-  }
+   // atmsp is using $ to mark a constant
+   for (std::size_t i=0; i<vExpr.size(); ++i)
+   {
+      PreprocessExpr(vExpr[i]);
+      //string s = vExpr[i];
+      //s = replaceAll(s, string("pi"), string("$pi"));
+      //s = replaceAll(s, string("-e*"), string("-$e*"));
+      //s = replaceAll(s, string("/e*"), string("/$e*"));
+      //s = replaceAll(s, string("(e*"), string("($e*"));
+      //s = replaceAll(s, string("-e/"), string("-$e/"));
+      //s = replaceAll(s, string("e^"), string("$e^"));
+      //s = replaceAll(s, string("+e)"), string("+$e)"));
+      //s = replaceAll(s, string("(e+"), string("($e+"));
+      //s = replaceAll(s, string("(e-"), string("($e-"));
+      //s = replaceAll(s, string("(-e"), string("(-$e"));
+      //s = replaceAll(s, string("-e)"), string("-$e)"));
+      //s = replaceAll(s, string("(e)"), string("($e)"));
+      //s = replaceAll(s, string("/e)"), string("/$e)"));
+      //s = replaceAll(s, string("/e/"), string("/$e/"));
+      //s = replaceAll(s, string("*e+"), string("*$e+"));
+      //vExpr[i] = s;
+   }
 }
 
 //-------------------------------------------------------------------------------------------------
@@ -85,34 +85,38 @@ void BenchATMSP::PreprocessExpr(std::string &s)
 //-------------------------------------------------------------------------------------------------
 double BenchATMSP::DoBenchmark(const std::string &sExpr, long iCount)
 {
-  ATMSB<double> bc;
+   ATMSB<double> bc;
 
-  // Parsing/bytecode generation with error check. In a scope here JUST to
-  // demonstrate that a parser-instance itself is NOT needed later on
-  ATMSP<double> p;
-  unsigned int err = p.parse(bc, sExpr, "a, b, c");
-  if (err)
-    throw std::runtime_error(std::string("atmsp: ") + p.errMessage(err));
+   // Parsing/bytecode generation with error check. In a scope here JUST to
+   // demonstrate that a parser-instance itself is NOT needed later on
+   ATMSP<double> p;
+   unsigned int err = p.parse(bc, sExpr, "a, b, c");
+   if (err)
+   {
+      StopTimer(std::numeric_limits<double>::max(),std::numeric_limits<double>::max(),0);
+      return std::numeric_limits<double>::max();
+      //throw std::runtime_error(std::string("atmsp: ") + p.errMessage(err));
+   }
 
-  // Set variable values for x,y,z. Here always 1,2,3
-  bc.var[0] = 1.0;
-  bc.var[1] = 2.0;
-  bc.var[2] = 3.0;
+   // Set variable values for x,y,z. Here always 1,2,3
+   bc.var[0] = 1.0;
+   bc.var[1] = 2.0;
+   bc.var[2] = 3.0;
 
-	// Calculate/bench and show result finally
-  double fTime(0);
-  double fRes(0), fSum(0);
+   // Calculate/bench and show result finally
+   double fTime(0);
+   double fRes(0), fSum(0);
 
-  fRes = bc.run();
+   fRes = bc.run();
 
-  StartTimer();
-  for (int j=0; j<iCount; j++) 
-  {
-    std::swap(bc.var[0], bc.var[1]);
-    fSum += bc.run();
-  }
-  
-  StopTimer(fRes, fSum, iCount);
+   StartTimer();
+   for (int j=0; j<iCount; j++)
+   {
+      std::swap(bc.var[0], bc.var[1]);
+      fSum += bc.run();
+   }
 
-  return m_fTime1;
+   StopTimer(fRes, fSum, iCount);
+
+   return m_fTime1;
 }
