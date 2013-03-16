e420f6f95c0601511ee68ed84cff029909b9d24a
partow <partow@5a969881-007f-eb33-5950-2d2997f90778>
2013-03-16 02:08:33 +0000

    1. Added MathExpr Parser 2. Updated the equality comparison for DNQ check

---
+++ BenchMathExpr.cpp
@@ -0,0 +1,79 @@
+#include "BenchMathExpr.h"
+
+#include <windows.h>
+#include <cmath>
+
+#include "MathExpr/mathexpr.h"
+
+using namespace std;
+
+
+//-------------------------------------------------------------------------------------------------
+BenchMathExpr::BenchMathExpr()
+: Benchmark()
+{
+   m_sName = "MathExpr";
+}
+
+//-------------------------------------------------------------------------------------------------
+double BenchMathExpr::DoBenchmark(const std::string &sExpr, long iCount)
+{
+   double a = 1.1;
+   double b = 2.2;
+   double c = 3.3;
+   double x = 1.1;
+   double y = 2.2;
+   double z = 3.3;
+   double w = 4.4;
+
+   double e  = 2.718281828459045235360;
+   double pi = 3.141592653589793238462;
+
+   RVar var_a ( "a" , &a );
+   RVar var_b ( "b" , &b );
+   RVar var_c ( "c" , &c );
+   RVar var_x ( "x" , &x );
+   RVar var_y ( "y" , &y );
+   RVar var_z ( "z" , &z );
+   RVar var_w ( "w" , &w );
+
+   RVar var_e  ( "e" , &a );
+   RVar var_pi ( "p" , &a );
+
+   RVar* var_array[9];
+
+   var_array[0] = &var_a;
+   var_array[1] = &var_b;
+   var_array[2] = &var_c;
+   var_array[3] = &var_x;
+   var_array[4] = &var_y;
+   var_array[5] = &var_z;
+   var_array[6] = &var_w;
+
+   var_array[7] = &var_e;
+   var_array[8] = &var_pi;
+
+   ROperation op (const_cast<char*>(sExpr.c_str()), 9, var_array);
+
+   if (op.HasError(&op))
+   {
+      StopTimer(std::numeric_limits<double>::max(),std::numeric_limits<double>::max(),0);
+      return std::numeric_limits<double>::max();
+   }
+
+   // Calculate/bench and show result finally
+   double fTime = 0;
+   double fRes  = 0;
+   double fSum  = 0;
+
+   fRes = op.Val();
+   StartTimer();
+   for (int j = 0; j < iCount; j++)
+   {
+      std::swap(a,b);
+      std::swap(x,y);
+      fSum += op.Val();
+   }
+   StopTimer(fRes, fSum, iCount);
+   return m_fTime1;
+}
---
+++ BenchMathExpr.h
@@ -0,0 +1,20 @@
+#ifndef BENCH_MATHEXPR_H
+#define BENCH_MATHEXPR_H
+
+#include <vector>
+#include <string>
+
+#include "Benchmark.h"
+
+//-------------------------------------------------------------------------------------------------
+class BenchMathExpr : public Benchmark
+{
+public:
+
+  BenchMathExpr();
+
+  double DoBenchmark(const std::string &sExpr, long iCount);
+
+};
+
+#endif
