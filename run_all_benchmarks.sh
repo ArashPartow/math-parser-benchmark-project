8efda4b21f0a0333024c60a2b05ac9288d753077
partow <partow@5a969881-007f-eb33-5950-2d2997f90778>
2013-03-29 03:01:22 +0000

    Added scripts to run all the benchmarks in one go.

---
+++ run_all_benchmarks.sh
@@ -0,0 +1,5 @@
+./ParserBench 100000 bench_expr.txt
+./ParserBench 100000 bench_expr_all.txt
+./ParserBench 100000 bench_expr_extensive.txt
+./ParserBench 100000 bench_expr_random.txt
+./ParserBench 100000 bench_precedence.txt
