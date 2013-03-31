cc19dba1b234736b73e934e34db5b66f6de46538
partow <partow@5a969881-007f-eb33-5950-2d2997f90778>
2013-03-31 02:25:48 +0000

    1. Updates to ExprTk library 2. Reordering of benchmark (longer ones last)

--- run_all_benchmarks.bat
+++ run_all_benchmarks.bat
@@ -1,5 +1,5 @@
 ParserBench.exe 100000 bench_expr.txt
 ParserBench.exe 100000 bench_expr_all.txt
-ParserBench.exe 100000 bench_expr_extensive.txt
 ParserBench.exe 100000 bench_expr_random.txt
+ParserBench.exe 100000 bench_expr_extensive.txt
 ParserBench.exe 100000 bench_precedence.txt
