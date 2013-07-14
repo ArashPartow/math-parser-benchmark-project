f9096f13bbd9ee1604f47cce3ec74114dca82e88
equinox.ib <equinox.ib@googlemail.com>
2013-07-14 10:34:03 +0000

    random function set with functions added

--- run_all_benchmarks.sh
+++ run_all_benchmarks.sh
@@ -1,6 +1,7 @@
 ./ParserBench.exe 100000 bench_expr.txt
 ./ParserBench.exe 100000 bench_expr_all.txt
-./ParserBench.exe 100000 bench_expr_random.txt
+./ParserBench.exe 100000 bench_expr_random_with_functions.txt
+./ParserBench.exe 100000 bench_expr_random_without_functions.txt
 ./ParserBench.exe 100000 bench_expr_extensive.txt
 ./ParserBench.exe 100000 bench_expr_precedence.txt
 ./ParserBench.exe 100000 bench_expr_weird.txt
