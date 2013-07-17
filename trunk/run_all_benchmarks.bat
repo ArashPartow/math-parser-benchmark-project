69387cbc113af5f7babf357d6ed88b12843a945a
partow <partow@5a969881-007f-eb33-5950-2d2997f90778>
2013-07-17 03:31:59 +0000

    1. Additional "weird" benchmark expressions. 2. Added bench_expr_complete.txt to the run_all scripts.

--- trunk/run_all_benchmarks.bat
+++ trunk/run_all_benchmarks.bat
@@ -5,3 +5,4 @@ ParserBench.exe 100000 bench_expr_random_without_functions.txt
 ParserBench.exe 100000 bench_expr_extensive.txt
 ParserBench.exe 100000 bench_expr_precedence.txt
 ParserBench.exe 100000 bench_expr_weird.txt
+ParserBench.exe 1000 bench_expr_complete.txt
