8174e580e97cb8e3aa7a7133e10d751f02e9630f
iberg <iberg@gmx.info>
2014-02-06 22:01:40 +0000

    linux binaries and project file for qt creator added

---
+++ run_all_benchmarks_linux.sh
@@ -0,0 +1,9 @@
+./math-parser-benchmark-project 1000000 bench_expr.txt
+./math-parser-benchmark-project 1000000 bench_expr_all.txt
+./math-parser-benchmark-project 1000000 bench_expr_random_with_functions.txt
+./math-parser-benchmark-project 1000000 bench_expr_random_without_functions.txt
+./math-parser-benchmark-project 1000000 bench_expr_extensive.txt
+./math-parser-benchmark-project 1000000 bench_expr_precedence.txt
+./math-parser-benchmark-project 1000000 bench_expr_weird.txt
+#./math-parser-benchmark-project 1000 bench_expr_complete.txt
+
