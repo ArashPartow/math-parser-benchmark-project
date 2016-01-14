44e611c48ff746403df604f5233fe0ec2c1e2c0d
schoetbi <schoetbi@schaeffler.com>
2016-01-14 12:04:51 +0100

    BenchLuajit.cpp Cleanup

--- src/BenchLuajit.cpp
+++ src/BenchLuajit.cpp
@@ -1,4 +1,5 @@
 #include "BenchLuaJit.h"
+#include <cstdio>
 #include <map>
 #include <lua.hpp>
 
@@ -9,40 +10,16 @@ BenchLuaJit::BenchLuaJit()
 	m_sName = "LuaJit";
 }
 
-template< class T>
-void swapVars(lua_State *L, const char *a, const char *b)
+void ErrorPrint(lua_State* L, int retVal)
 {
-	double temp;
-	// save a to temp
-	lua_getglobal(L, a);
-	temp = lua_tonumber(L, -1);
-	// a = b
-	lua_getglobal(L, b);
-	lua_setglobal(L, a);
-	// b = a
-	lua_pushnumber(L, temp);
-	lua_setglobal(L, b);
-}
+	if (retVal == 0)
+	{
+		return;
+	}
 
-// Catch C++ exceptions and convert them to Lua error messages.
-// Customize as needed for your own exception classes.
-static int fnktWithSwap(lua_State *L, lua_CFunction f)
-{
-	try {
-		return f(L);  // Call wrapped function and return result.
-	}
-	catch (const char *s) {  // Catch and convert exceptions.
-		lua_pushstring(L, s);
-	}
-	catch (std::exception& e) {
-		lua_pushstring(L, e.what());
-	}
-	catch (...) {
-		lua_pushliteral(L, "caught (...)");
-	}
-	return lua_error(L);  // Rethrow as a Lua error.
+	printf("%s\n", lua_tostring(L, -1));
+	lua_pop(L, 1);
 }
-
 
 //-------------------------------------------------------------------------------------------------
 double BenchLuaJit::DoBenchmark(const std::string& sExpr, long iCount)
@@ -63,18 +40,20 @@ double BenchLuaJit::DoBenchmark(const std::string& sExpr, long iCount)
 	try
 	{		
 		L = luaL_newstate();
-		
+				
 		for (auto it = var_list.begin(); it != var_list.end(); ++it)
 		{
 			lua_pushnumber(L, it->second);
 			lua_setglobal(L, it->first.c_str());
-		}
+		}		
 
-		luaL_dostring(L, ("function fnkt() return " + sExpr + "; end").c_str());
-		luaL_dostring(L, ("function fnktWithSwap() local result = " + sExpr + "; a,b = b,a; x,y = y,x; return result; end").c_str());
-				
-		//luaL_dostring(L, ("function fnktWithSwap2() result = " + sExpr + "; a,b = b,a; x,y = y,x; end").c_str());
-		luaL_dostring(L, "function swapall() a,b = b,a; x,y = y,x; end");
+		int result = luaJIT_setmode(L, 0, LUAJIT_MODE_ENGINE);
+		ErrorPrint(L, luaL_dostring(L, ("function fnkt() return " + sExpr + "; end").c_str()));
+		ErrorPrint(L, luaL_dostring(L, 
+			("function fnktWithSwap() local result = " + sExpr + 
+			"  a,b = b,a; x,y = y,x; "
+			"  return result; "
+			"end").c_str()));
 	}
 	catch (std::exception& e)
 	{
@@ -91,8 +70,6 @@ double BenchLuaJit::DoBenchmark(const std::string& sExpr, long iCount)
 	fRes = lua_tonumber(L, -1);
 	lua_pop(L, 1);
 
-	
-
 	StartTimer();
 
 	for (int j = 0; j < iCount; ++j)
@@ -102,14 +79,6 @@ double BenchLuaJit::DoBenchmark(const std::string& sExpr, long iCount)
 		double result = lua_tonumber(L, -1);
 		fSum += result;
 		lua_pop(L, 1);
-
-		// swap variables
-		//swapVars<int>(L, "a", "b");
-		//swapVars<int>(L, "x", "y");
-		
-		// swap by lua function call (slower)
-		// lua_getglobal(L, "swapall");
-		// lua_pcall(L, 0, 0, 0);
 	}
 
 	StopTimer(fRes, fSum, iCount);
